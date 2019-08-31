1145e8b09b48335de293eecda050c8230099a16e
Neko3000 <Neko3000@live.com>
2019-08-31 20:29:03 +0900

    feat(ItemDetailViewController): finish animation

--- Zizhi.iOS/Controllers/ItemDetailViewController.h
+++ Zizhi.iOS/Controllers/ItemDetailViewController.h
@@ -10,7 +10,12 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ItemDetailViewController : UIViewController
+@interface ItemDetailViewController : UIViewController{
+    UISwipeGestureRecognizer* swipeGestureReconginzerUp;
+    UISwipeGestureRecognizer* swipeGestureReconginzerRight;
+    
+    BOOL toFinalStatus;
+}
 
 @end
 
--- Zizhi.iOS/Controllers/ItemDetailViewController.m
+++ Zizhi.iOS/Controllers/ItemDetailViewController.m
@@ -10,7 +10,7 @@
 #import "ImageGalleryView.h"
 #import "CALayer+SketchShadow.h"
 
-@interface ItemDetailViewController ()
+@interface ItemDetailViewController () <UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet ImageGalleryView *imageGalleryView;
 
@@ -18,8 +18,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *itemShortDescritptionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *itemPriceLabel;
 @property (weak, nonatomic) IBOutlet UIButton *shareBtn;
+@property (weak, nonatomic) IBOutlet UIImageView *splitterImageView;
 @property (weak, nonatomic) IBOutlet UIView *headerBlockShadowCaster;
-
+@property (weak, nonatomic) IBOutlet UIView *headerBlock;
 
 @property (weak, nonatomic) IBOutlet UILabel *itemSellerNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *itemTagLabel;
@@ -29,9 +30,12 @@
 @property (weak, nonatomic) IBOutlet UIImageView *editorAvatarImageView;
 @property (weak, nonatomic) IBOutlet UIView *footerBlockShadowCaster;
 
-
 @property (weak, nonatomic) IBOutlet UIScrollView *mainScrollView;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageGalleryViewConstraintTop;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerConstraintTopSpacingToImageGalleryView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bodyConstraintTopSpacingToHeader;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *footerConstraintTopSpcaingToBody;
 
 @end
 
@@ -54,6 +58,7 @@
     [self.headerBlockShadowCaster.layer applySketchShadow:[UIColor colorNamed:@"Shadow-LightBlue"] alpha:1.0 x:0 y:-12 blur:16.0 spread:0];
     self.itemPriceLabel.layer.anchorPoint = CGPointMake(0.5, 0);
     
+    
     // Body
     self.itemDescriptionTextView.text = self.itemDescriptionTextView.text;
     [self.itemDescriptionTextView setSelectable:NO];
@@ -65,6 +70,25 @@
     self.footerBlockShadowCaster.layer.cornerRadius = 6.0;
     [self.footerBlockShadowCaster.layer applySketchShadow:[UIColor colorNamed:@"Shadow-LightBlue"] alpha:1.0 x:0 y:12.0 blur:16.0 spread:0];
     
+    // ScrollView
+    [self.mainScrollView setScrollEnabled:NO];
+    self.mainScrollView.delegate = self;
+    
+    // GesutreReconginzer
+    swipeGestureReconginzerUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGestureReconginzerHandler:)];
+    swipeGestureReconginzerUp.direction = UISwipeGestureRecognizerDirectionUp;
+    [self.view addGestureRecognizer:swipeGestureReconginzerUp];
+    
+    swipeGestureReconginzerRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGestureReconginzerHandler:)];
+    swipeGestureReconginzerRight.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipeGestureReconginzerRight];
+    
+    [self.view setUserInteractionEnabled:YES];
+}
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.headerConstraintTopSpacingToImageGalleryView.constant = self.view.safeAreaLayoutGuide.layoutFrame.size.height - self.imageGalleryView.frame.origin.y - self.imageGalleryView.frame.size.height - self.headerBlock.frame.size.height;
 }
 
 /*
@@ -76,5 +100,81 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void) playHeaderAnimation:(BOOL)toFinalStatus duration:(float)duration{
+    
+    CGFloat priceLabelScaleRatio = 0.674;
+    
+    if(toFinalStatus){
+        
+        [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.itemPriceLabel.transform = CGAffineTransformMakeScale(priceLabelScaleRatio, priceLabelScaleRatio);
+            self.splitterImageView.alpha = 0;
+        } completion:^(BOOL finished){
+            
+        }];
+    }
+    else{
+        [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^(){
+            self.itemPriceLabel.transform = CGAffineTransformMakeScale(1.0, 1.0);
+            self.splitterImageView.alpha = 1.0;
+        } completion:^(BOOL finished){
+            
+        }];
+        
+    }
+    
+}
+
+- (void) transition:(BOOL)toFinalStatus{
+    if(toFinalStatus){
+        self.imageGalleryViewConstraintTop.constant = -265 -20;
+        self.bodyConstraintTopSpacingToHeader.constant = -59;
+        self.footerConstraintTopSpcaingToBody.constant = 13;
+        
+        [self playHeaderAnimation:YES duration:0.7];
+        
+        [UIView animateWithDuration:0.7 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            
+            [self.view layoutIfNeeded];
+        } completion:^(BOOL finished){
+            
+            [self.mainScrollView setScrollEnabled:YES];
+            [self->swipeGestureReconginzerUp setEnabled:NO];
+        }];
+    }
+    else{
+        self.imageGalleryViewConstraintTop.constant = 13;
+        self.bodyConstraintTopSpacingToHeader.constant = -59 + 100;
+        self.footerConstraintTopSpcaingToBody.constant = 13 + 100;
+        
+        [self playHeaderAnimation:NO duration:0.7];
+        
+        [UIView  animateWithDuration:0.7 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            
+            [self.view layoutIfNeeded];
+        } completion:^(BOOL finished){
+            
+            [self.mainScrollView setScrollEnabled:NO];
+            [self->swipeGestureReconginzerUp setEnabled:YES];
+        }];
+    }
+}
+
+- (void) swipeGestureReconginzerHandler:(UISwipeGestureRecognizer*)swipeGestureReconginzer{
+    if(swipeGestureReconginzer.direction == UISwipeGestureRecognizerDirectionUp){
+        [self transition:YES];
+    }
+    else if(swipeGestureReconginzer.direction == UISwipeGestureRecognizerDirectionRight){
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    if(scrollView.contentOffset.y <= -20){
+        [self transition:NO];
+    }
+}
 
 @end
